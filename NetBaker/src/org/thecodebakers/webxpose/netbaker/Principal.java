acad892ec801bdbe61d6dbe183e22bf3f3bdc118
cleuton.sampaio <cleuton.sampaio@gmail.com>
2011-08-28 11:04:56 +0000

    Versão inicial para desenvolvimento

---
+++ NetBaker/src/org/thecodebakers/webxpose/netbaker/Principal.java
@@ -0,0 +1,13 @@
+package org.thecodebakers.webxpose.netbaker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Principal extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
