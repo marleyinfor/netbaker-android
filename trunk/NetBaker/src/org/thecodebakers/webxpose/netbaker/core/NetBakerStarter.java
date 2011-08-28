60d618be68b0640d2df1de36da8cf0746be14941
cleuton.sampaio@gmail.com <cleuton.sampaio@gmail.com@fe6d83f6-6016-0223-3f9c-d2f71c59a909>
2011-08-28 11:04:56 +0000

    Versão inicial para desenvolvimento

---
+++ trunk/NetBaker/src/org/thecodebakers/webxpose/netbaker/core/NetBakerStarter.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2011 The Code Bakers
+ * Authors: Cleuton Sampaio e Francisco Rodrigues
+ * e-mail: thecodebakers@gmail.com
+ * Project: http://code.google.com/p/netbaker-android
+ * Site: http://www.thecodebakers.org
+ *
+ * Licensed under the GNU LGPL, Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/lgpl.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * The over the LGPL V 3 there is the following addition: 
+ * - The authors may create another version of the program which will be distributed
+ *   as proprietary software.
+ * 
+ * @author Cleuton Sampaio e Francisco Rogrigues - thecodebakers@gmail.com
+ */
+package org.thecodebakers.webxpose.netbaker.core;
+
+import android.content.Context;
+
+public class NetBakerStarter {
+	private static Context context;
+	
+}
