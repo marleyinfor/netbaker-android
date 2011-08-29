ea06dbc1a30a77a743c754b174bd1065dba905c6
cleuton.sampaio@gmail.com <cleuton.sampaio@gmail.com@fe6d83f6-6016-0223-3f9c-d2f71c59a909>
2011-08-29 12:16:42 +0000

    better save then sorry

--- trunk/NetBaker/src/org/thecodebakers/webxpose/netbaker/core/NetBakerService.java
+++ trunk/NetBaker/src/org/thecodebakers/webxpose/netbaker/core/NetBakerService.java
@@ -29,23 +29,162 @@ import java.net.ServerSocket;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.thecodebakers.webxpose.netbaker.R;
+import org.thecodebakers.webxpose.netbaker.core.NetBakerServerService.ProcessAdminRequests;
+import org.thecodebakers.webxpose.netbaker.core.NetBakerServerService.ProcessWebRequests;
+
 import android.app.Service;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.Resources;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
 
 public class NetBakerService extends Service {
 
-	private int porta;
-	private int portaAdm;
+	protected Resources res;
+	protected int porta;
+	protected int portaAdm;
 	protected ServerSocket serverSocket;
 	protected ServerSocket serverAdmSocket;
-	private Thread mainProcessThread;
-	private Thread adminProcessThread;
-	private final String TAG = "NetBaker";
-	private String serverName;
-	private static NetBakerService selfRef;
-	protected boolean log = true;
-	protected c
+	protected Thread mainProcessThread;
+	protected Thread adminProcessThread;
+	protected final String TAG = "NetBaker";
+	protected String serverName;
+	protected static NetBakerService selfRef;
+	protected boolean debug = true;
+	protected boolean verbose = true;
+	protected boolean toastError = false;
+	protected String protocolClassName = null;
+	protected InetBakerProtocol protocol = null;
+
+	// Constants
+	protected static enum MSGTYPE {TINFO, TDEBUG, TWARN, TERROR};
+	
+	// Main method
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		res = this.getResources();
+		if (this.serverName.length() == 0) {
+			serverName = TAG;
+		}
+		this.msg(MSGTYPE.TINFO, this.getProps(R.string.servidorIniciando, 1));
+		parseExtras(intent);
+
+		try {
+			serverSocket = new ServerSocket(porta);
+			try {
+				serverAdmSocket = new ServerSocket(portaAdm);
+				ProcessWebRequests procReq = new ProcessWebRequests(serverSocket);
+				mainProcessThread = new Thread(procReq);
+				mainProcessThread.start();
+				ProcessAdminRequests procAdm = new ProcessAdminRequests(serverAdmSocket);
+				adminProcessThread = new Thread(procAdm);
+				adminProcessThread.start();
+				// Is good when everything runs ok...
+				this.msg(MSGTYPE.TINFO, this.getProps(R.string.onstartok, 2));
+				return START_STICKY;
+			}
+			catch (Exception ex) {
+				this.msg(MSGTYPE.TERROR, this.getProps(R.string.errorOpeningAdminPort, 3));
+			}
+		}
+		catch (Exception ex) {
+			this.msg(MSGTYPE.TERROR, this.getProps(R.string.errorOpeningPort, 4));
+		}
+		return START_NOT_STICKY;
+	}
+	
+	// Main request processing thread
+	
+	// Main request admin processing thread
+
+	// Utility
+	
+	private boolean parseExtras(Intent intent) {
+		boolean resultado = true;
+		Bundle extras = intent.getExtras();
+		try {
+			this.porta = Integer.parseInt(extras.getString("port"));
+			this.portaAdm = Integer.parseInt(extras.getString("adminPort"));
+		}
+		catch (NumberFormatException nfe) {
+			this.msg(MSGTYPE.TERROR, this.getProps(R.string.nonNumericPorts, 5));
+			resultado = false;
+		}
+		this.serverName = extras.getString("serverName");
+		this.debug = extras.getBoolean("debug");
+		this.verbose = extras.getBoolean("verbose");
+		this.toastError = extras.getBoolean("toastError");
+		this.protocolClassName = extras.getString("protocolClassName");
+		if (this.protocolClassName == null) {
+			this.msg(MSGTYPE.TERROR, this.getProps(R.string.missingProtocolClassName, 6));
+			resultado = false;
+		}
+		else {
+			try {
+				Class.forName(this.protocolClassName);
+				try {
+					this.protocol = (InetBakerProtocol) Class.forName(this.protocolClassName).newInstance();
+				}
+				catch (ClassCastException cce) {
+					this.msg(MSGTYPE.TERROR, this.getProps(R.string.cannotCastProtocol, 10));
+					resultado = false;
+				} catch (IllegalAccessException e) {
+					this.msg(MSGTYPE.TERROR, this.getProps(R.string.cannotAccessProtocol, 8));
+					resultado = false;
+				} catch (InstantiationException e) {
+					this.msg(MSGTYPE.TERROR, this.getProps(R.string.cannotInstantiateProtocol, 9));
+					resultado = false;
+				}
+			}
+			catch (ClassNotFoundException cnf) {
+				this.msg(MSGTYPE.TERROR, this.getProps(R.string.missingProtocolClass, 7));
+				resultado = false;
+			}
+		}
+		return resultado;
+	}
+	
+	protected void msg(MSGTYPE tipo, String mensagem) {
+		switch(tipo) {
+		case TINFO:
+			if (this.verbose) {
+				Log.i(this.TAG, mensagem);
+			}
+		case TWARN:
+			if (this.verbose) {
+				Log.w(this.TAG, mensagem);
+			}
+		case TDEBUG:
+			if (this.debug) {
+				Log.d(this.TAG, mensagem);
+			}
+		case TERROR:
+			Log.e(this.TAG, mensagem);
+			if (this.toastError) {
+				Toast.makeText(this.getApplicationContext(), mensagem, Toast.LENGTH_LONG);
+			}
+		}
+	}
+	
+	protected String getProps(int id, int sit) {
+		String retorno = null;
+		try {
+			retorno = res.getString(id);
+		}
+		catch(Resources.NotFoundException rnf) {
+			retorno = "message " + sit;
+		}
+		return retorno;
+	}
+	
+
+	// Other
 	
 	@Override
 	public IBinder onBind(Intent arg0) {
@@ -53,4 +192,5 @@ public class NetBakerService extends Service {
 		return null;
 	}
 
+	
 }
